e6f4ec2ee9ab79fbdd365ffef3f345842511fdfc
Luis <luisfernandogvv@gmail.com>
2023-01-09 10:08:15 -0300

    feat: inserting new errors and readline

--- src/index.js
+++ src/index.js
@@ -1,22 +1,58 @@
 import { ChatGPTAPIBrowser } from 'chatgpt';
+import readline from 'readline';
 import { Client } from 'whatsapp-web.js';
 import qrcode from 'qrcode-terminal';
 import dotenv from 'dotenv';
+import path from 'path';
 
 dotenv.config();
 
-async function Start(){
+const read = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function StartQuestion(){
+    read.question('🤖: Você deseja que o BOT esteja disponível exclusivamente para você ou para seus contatos ? \n 1 - Meus Contatos \n 2 - Para mim \n Modo: ',insertMode);
+}
+
+StartQuestion();
+
+async function insertMode( response ){
+
+    const validResponses = ['1','2'];
+
+    if( !validResponses.includes(response) ){
+        console.log('🤖: Opção inválida !');
+        return StartQuestion();
+    }
+
+    const mode = {
+        '1': 'message',
+        '2': 'message_create'
+    };
 
     const browser = new ChatGPTAPIBrowser({
         email: process.env.CHATGPT_EMAIL,
         password: process.env.CHATGPT_PASSWORD,
         isGoogleLogin:true,
+        executablePath: path.join("C:/Program Files (x86)/Google/Chrome/Application/chrome.exe"),
+        minimize:false,
     });
 
-    console.log('🤖: Aguarde um pouco enquanto preparamo tudo.')
-    
-    await browser.initSession();
 
+    console.log('🤖: Aguarde um pouco enquanto preparamos tudo.')
+    
+    try{
+
+        await browser.initSession();
+
+    }catch(err){
+        
+        console.log('🤖: Ops ! isso está demorando mais que o normal. Mas não se preocupe. Irei trabalhar nisso');
+
+    }
+    
     const whats = new Client();
 
     whats.initialize();
@@ -31,7 +67,7 @@ async function Start(){
 
     whats.on('ready', () => console.log('🤖: Estou pronto para ser utilizado !'));
 
-    whats.on('message_create',sendGPTMessage);
+    whats.on(mode[response],sendGPTMessage);
 
 
     async function sendGPTMessage(message){
@@ -50,7 +86,7 @@ async function Start(){
 
                }catch(err){
 
-                    whats.sendMessage(message.from,'🤖: Desculpe. Algum erro ocorreu 🥲. Tente novamente mais tarde, ok ? 😊');
+                    whats.sendMessage(message.from,'🤖: Desculpe. Algum erro ocorreu durante minha digitação 🥲.');
 
                }
     
@@ -58,15 +94,12 @@ async function Start(){
 
             gpt();
 
-            whats.sendMessage(message.from,`🤖: Aguarde um instante. Pode não parecer, mas estou digitando 😁👽`);
+            whats.sendMessage(message.from,`🤖: Aguarde um instante. Pode não parecer, mas estou digitando 😁`);
 
         }
     }
 
-
 }
-
-Start();
 
 
 
