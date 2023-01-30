556664e850529bfd74c2a0a977e021cb1e044247
Luis <luisfernandogvv@gmail.com>
2023-01-30 12:56:01 -0300

    feat: Adiconando novos estados ao type alias "States"

--- src/types/alias/States.ts
+++ src/types/alias/States.ts
@@ -1,2 +1,2 @@
 
-export type State = 'welcome' | 'after-select-option' | 'before-select-option' | 'session';
+export type State = 'welcome' | 'after-select-option' | 'before-select-option' | 'session' | 'choice-bot-name';
