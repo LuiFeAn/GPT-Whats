684657e6caf8b4ce393690ec95d1f7e21f095a3d
Luis <luisfernandogvv@gmail.com>
2023-02-02 08:44:30 -0300

    feat: Criando método responsável pela criação de um novo registro de sessão ao banco de dados

--- src/services/sessionService.ts
+++ src/services/sessionService.ts
@@ -3,6 +3,15 @@ import AppDataSource from "../database/dbConfig.js";
 import { Repository } from "typeorm";
 
 
+interface ICreateSession {
+
+    sessionId: string;
+    messageId: string;
+    conversationId: string
+
+}
+
+
 class SessionService {
 
     public repository: Repository<Session>
@@ -13,10 +22,22 @@ class SessionService {
 
     }
 
+    async createSession({ sessionId, messageId, conversationId }: ICreateSession){
+
+        const session = this.repository.create({
+            session_id: sessionId,
+            conversation_id: conversationId,
+            message_id: messageId
+        });
+
+        await this.repository.save(session)
+
+    }
+
     async findSession(id: string){
 
         const session = await this.repository.findOneBy({
-            sessionId: id
+            session_id: id
         });
 
         return session;
