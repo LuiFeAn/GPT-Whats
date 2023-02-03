32b33043f3e1e445b214cb1ce1921a7a6f2ed6b8
Luis <luisfernandogvv@gmail.com>
2023-02-03 10:38:26 -0300

    refactor: Removendo props "state" e "sessions"

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,12 +1,8 @@
-import { State } from "../types/alias/States.js";
-import Session from "../models/Session.js";
 import { Options } from "../types/alias/Options.js";
 
 export interface IUser {
 
     readonly phone: string,
     message: string | Options,
-    state: State,
-    sessions: Session [],
 
 }
